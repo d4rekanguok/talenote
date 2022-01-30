cad4ba9db65dcf55571cc70a0eb48f669c82eba0
Derek Nguyen <dikanguok@gmail.com>
2022-01-30 21:35:26 +0900

    chore: remove todo page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -34,11 +34,6 @@
 		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
 		the devtools network panel and reloading.
 	</p>
-
-	<p>
-		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
-		it with JavaScript disabled!
-	</p>
 </div>
 
 <style>
